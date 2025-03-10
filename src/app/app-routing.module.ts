724b907800cfe927c11b486be5164c9274c05bed
leo-mutuku <le.mutuku@gmail.com>
2025-03-10 23:51:40 +0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"dashboard",component:DashboardComponent},
+  {path:"about", component:AboutComponent},
+  {path:"", component:DashboardComponent, pathMatch:"full"}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
